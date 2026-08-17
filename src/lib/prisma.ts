e2c7a0229d0d9cadc59c7cdaab5273a4c7e314c4
sofiahaedo <haedossofia@gmail.com>
2026-08-17 19:46:08 -0300

    funcionaaaaa-version oficial

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,31 @@
-import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+// import { PrismaClient } from "@/generated/prisma/client";
+// //import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+
+// const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+// function createClient() {
+//   const url = process.env.DATABASE_URL;
+//   if (!url) throw new Error("DATABASE_URL no está definida");
+//   return new PrismaClient({ adapter: new PrismaMariaDb(url) });
+// }
+
+// export const prisma = globalForPrisma.prisma ?? createClient();
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// export default prisma;
+
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-function createClient() {
-  const url = process.env.DATABASE_URL;
-  if (!url) throw new Error("DATABASE_URL no está definida");
-  return new PrismaClient({ adapter: new PrismaMariaDb(url) });
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
 
-export const prisma = globalForPrisma.prisma ?? createClient();
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 export default prisma;
